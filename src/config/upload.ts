4010a5c11e782232556f0704842209ec310367dc
Guilherme Nisiyama de Jesus <guilherme.nisiyama@gmail.com>
2020-06-11 00:22:37 -0300

    Atualização: Implementação do Amazon S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,13 +1,33 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer,{ StorageEngine} from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  }
+
+  config: {
+    disk: {},
+    aws: {
+      bucket:string;
+    },
+  }
+}
+
 export default {
+  driver:process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
+multer: {
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
@@ -16,4 +36,12 @@ export default {
       return callback(null, fileName);
     },
   }),
-};
+},
+
+  config : {
+    disk: {},
+    aws:{
+      bucket: 'app-gobarber-nisiybr'
+    },
+  },
+} as IUploadConfig;
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,8 +1,10 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from './models/IStorageProvider';
 
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+import S3DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/S3DiskStorageProvider';
 
 
 
@@ -10,8 +12,9 @@ import DiskStorageProvider from '@shared/container/providers/StorageProvider/imp
 
 const providers =  {
   disk: DiskStorageProvider,
+  s3: S3DiskStorageProvider,
 }
 container.registerSingleton<IStorageProvider>(
   'DiskStorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
