1495061158a8f9b47d2155b78fa073615e9360e5
Guilherme Nisiyama de Jesus <guilherme.nisiyama@gmail.com>
2020-06-13 14:11:33 -0300

    Cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,7 +2,10 @@ import User from '@modules/users/infra/typeorm/entities/Users';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import AppError from '@shared/errors/AppError';
+
 
 import { injectable, inject } from 'tsyringe';
 
@@ -18,6 +21,8 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,    
   ) {}
 
   public async execute({ name, email, password }: IRequestDTO): Promise<User> {
@@ -34,6 +39,9 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list')
+
     return user;
   }
 }
